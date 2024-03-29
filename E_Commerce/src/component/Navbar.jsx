82bd18e80c3d80d1a3c548d5b3c31af658ad7a39
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-03-29 17:29:37 +0530

    routing update

--- E_Commerce/src/component/Navbar.jsx
+++ E_Commerce/src/component/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
 import {
     Flex,
     Text
 } from  '@chakra-ui/react'
+import { AuthContext } from '../context/authcontext'
 const Navbar = () => {
+  const {isAuth}=useContext(AuthContext);
   return (
     <>
       <Flex w={"90%"} m={"10px auto"} bg={"blue.300"} borderRadius={"4px"} justifyContent={"space-around"}>
-        <Link to={"/home"}><Text color="black" fontSize={"22px"} fontWeight={500}>Home</Text></Link>
+        <Link to={isAuth?"/":"/login"}><Text color="black" fontSize={"22px"} fontWeight={500}>Home</Text></Link>
         <Link to={"/login"}><Text color="black" fontSize={"22px"} fontWeight={500}>Login</Text></Link>
-        <Link to={"/products"}><Text color="black" fontSize={"22px"} fontWeight={500}>Products</Text></Link>
+        <Link to={isAuth?"/products":"/login"}><Text color="black" fontSize={"22px"} fontWeight={500}>Products</Text></Link>
       </Flex>
     </>
   )
--- E_Commerce/src/pages/Products.jsx
+++ E_Commerce/src/pages/Products.jsx
@@ -1,10 +1,71 @@
-import { Text } from '@chakra-ui/react'
 import React from 'react'
-
+import {
+  Box,
+  Button,
+  Flex,
+  Switch,
+  Text,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  useToast,
+} from "@chakra-ui/react";
 const Products = () => {
   return (
     <>
-     <Text>Products</Text> 
+     <Flex w={"90%"} gap={10} m={"0px auto 30px"} >
+        <Menu closeOnSelect={true}>
+          <MenuButton as={Button} colorScheme="blue">
+            Filter by Status
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup
+              title="Select Below"
+              defaultValue=""
+              type="radio"
+              onChange={(e) => setStatusFilter(e)}
+            >
+              <MenuItemOption value="completed">Completed</MenuItemOption>
+              <MenuItemOption value="incomplete">Incomplete</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+        <Menu closeOnSelect={true}>
+          <MenuButton as={Button} colorScheme="pink">
+            Filter by Assigned to
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup
+              title="Select Below"
+              defaultValue=""
+              type="radio"
+              onChange={(e) => setAssigneeFilter(e)}
+            >
+              <MenuItemOption value="Murtaza">Murtaza</MenuItemOption>
+              <MenuItemOption value="Rahul">Rahul</MenuItemOption>
+              <MenuItemOption value="Shubham">Shubham</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+        <Menu closeOnSelect={true}>
+          <MenuButton as={Button} colorScheme="green">
+            Sort by Date
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup
+              title="Select Below"
+              defaultValue=""
+              type="radio"
+              onChange={(e) => setSortByDate(e)}
+            >
+              <MenuItemOption value="asc">Newest</MenuItemOption>
+              <MenuItemOption value="desc">Oldest</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </Flex>
     </>
   )
 }
